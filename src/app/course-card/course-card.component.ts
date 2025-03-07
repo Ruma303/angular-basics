2d65181ee42946cf3236f0c40cc4b8b934d8ed0e
Ruma <matteoh8@hotmail.it>
2025-03-07 17:59:16 +0100

    .

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -9,34 +9,6 @@ import { Course } from '../interfaces/course';
 })
 export class CourseCardComponent {
 
-    // Evitiamo di usare @Input() per ogni proprietà
-    /* @Input()
-    course!: Course; */
-
-    // Altra alternativa più verbosa per ogni proprietà
-    /* @Input()
-    id: number = 0;
-    @Input()
-    name: string =  "Default name";
-    @Input()
-    description: string = "Description";
-    @Input()
-    price: number = 0.00;
-    @Input()
-    imageUrl: string = "https://via.placeholder.com/150";
-    @Input()
-    duration: string = "00:00:00";
-    @Input()
-    level: string = "Beginner";
-    @Input()
-    instructor: string = "John Doe";
-    @Input()
-    rating: number = 0;
-    @Input()
-    enrolledStudents: number = 0; */
-
-
-    // In alternativa possiamo usare @Input() con valori predefiniti
     @Input()
     course: Course = {
         id: 0,
